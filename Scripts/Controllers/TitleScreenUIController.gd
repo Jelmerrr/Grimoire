60b534cdc705c7e775fe470970eddc1b7cf9f54c
Jelmerrr <Jelljel@hotmail.com>
2025-11-10 20:15:23 +0100

    Title screen and scene manager

---
+++ Scripts/Controllers/TitleScreenUIController.gd
@@ -0,0 +1,20 @@
+extends Control
+
+func _on_start_game_button_pressed() -> void:
+	UtilsSceneManager.switch_scene(UtilsSceneManager.BOARD_SCENE)
+
+
+func _on_options_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_library_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_achievements_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_quit_game_button_pressed() -> void:
+	get_tree().quit()
---
+++ Scripts/Globals/Utils/UtilsSceneManager.gd
@@ -0,0 +1,21 @@
+extends Node
+
+const BOARD_SCENE = preload("uid://bdx3eg3w0wwmw")
+const TITLE_SCREEN_SCENE = preload("uid://vl7uj8cdqhru")
+
+var current_scene = null
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var root = get_tree().root
+	current_scene = root.get_child(root.get_child_count() - 1)
+
+func switch_scene(res_path):
+	call_deferred("deferred_switch_scene", res_path)
+
+func deferred_switch_scene(res_path):
+	current_scene.free()
+	var s = res_path
+	current_scene = s.instantiate()
+	get_tree().root.add_child(current_scene)
+	get_tree().current_scene = current_scene
