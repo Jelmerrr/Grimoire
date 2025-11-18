5d05ed4704da4a3a686e2222a3b2abcb5c134bd0
Jelmerrr <Jelljel@hotmail.com>
2025-11-19 00:11:34 +0100

    Some UI Tweaks

--- Scripts/Controllers/EnemyTooltipUIController.gd
+++ Scripts/Controllers/EnemyTooltipUIController.gd
@@ -2,10 +2,17 @@ extends PanelContainer
 
 @onready var hp_bar: ProgressBar = $VBoxContainer/HpBar
 
+@onready var hp_bar_label: RichTextLabel = $VBoxContainer/HpBar/HpBarLabel
+@onready var enemy_name_label: RichTextLabel = $VBoxContainer/EnemyNameLabel
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
 
+
+func update_details(currentHealth: int, enemyResource: BaseEnemyResource) -> void:
+	enemy_name_label.text = enemyResource.enemyName
+	hp_bar_label.text = str(currentHealth) + " / " + str(enemyResource.maxHealth)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
--- Scripts/Controllers/NewPageSelectionUIController.gd
+++ Scripts/Controllers/NewPageSelectionUIController.gd
@@ -1,14 +1,14 @@
 extends Control
 
-@onready var page_1_title: RichTextLabel = $"HBoxPanels/New Page 1 Container/MarginContainer/VBoxContainer/Page 1 Title"
-@onready var page_1_description: RichTextLabel = $"HBoxPanels/New Page 1 Container/MarginContainer/VBoxContainer/Page 1 Description"
-@onready var page_2_title: RichTextLabel = $"HBoxPanels/New Page 2 Container/MarginContainer/VBoxContainer/Page 2 Title"
-@onready var page_2_description: RichTextLabel = $"HBoxPanels/New Page 2 Container/MarginContainer/VBoxContainer/Page 2 Description"
-@onready var page_3_title: RichTextLabel = $"HBoxPanels/New Page 3 Container/MarginContainer/VBoxContainer/Page 3 Title"
-@onready var page_3_description: RichTextLabel = $"HBoxPanels/New Page 3 Container/MarginContainer/VBoxContainer/Page 3 Description"
-@onready var new_page_1_container: PanelContainer = $"HBoxPanels/New Page 1 Container"
-@onready var new_page_2_container: PanelContainer = $"HBoxPanels/New Page 2 Container"
-@onready var new_page_3_container: PanelContainer = $"HBoxPanels/New Page 3 Container"
+@onready var page_1_title: RichTextLabel = $"VBoxContainer/HBoxPanels/New Page 1 Container/MarginContainer/VBoxContainer/Page 1 Title"
+@onready var page_1_description: RichTextLabel = $"VBoxContainer/HBoxPanels/New Page 1 Container/MarginContainer/VBoxContainer/Page 1 Description"
+@onready var page_2_title: RichTextLabel = $"VBoxContainer/HBoxPanels/New Page 2 Container/MarginContainer/VBoxContainer/Page 2 Title"
+@onready var page_2_description: RichTextLabel = $"VBoxContainer/HBoxPanels/New Page 2 Container/MarginContainer/VBoxContainer/Page 2 Description"
+@onready var page_3_title: RichTextLabel = $"VBoxContainer/HBoxPanels/New Page 3 Container/MarginContainer/VBoxContainer/Page 3 Title"
+@onready var page_3_description: RichTextLabel = $"VBoxContainer/HBoxPanels/New Page 3 Container/MarginContainer/VBoxContainer/Page 3 Description"
+@onready var new_page_1_container: PanelContainer = $"VBoxContainer/HBoxPanels/New Page 1 Container"
+@onready var new_page_2_container: PanelContainer = $"VBoxContainer/HBoxPanels/New Page 2 Container"
+@onready var new_page_3_container: PanelContainer = $"VBoxContainer/HBoxPanels/New Page 3 Container"
 
 var page1selected: bool = false
 var page2selected: bool = false
