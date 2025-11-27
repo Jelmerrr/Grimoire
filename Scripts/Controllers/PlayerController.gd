13db76790a3cb7231166aa1dddff070b6aba6072
Jelmerrr <Jelljel@hotmail.com>
2025-11-28 00:37:14 +0100

    Animated player sprite

--- Scripts/Controllers/PlayerController.gd
+++ Scripts/Controllers/PlayerController.gd
@@ -1,11 +1,13 @@
 extends Node2D
 
 @onready var health_bar: ProgressBar = $"Health Bar"
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	SignalBus.Ask_PlayerPos.connect(UpdatePlayerPos)
 	health_bar.value = health_bar.max_value
+	animated_sprite_2d.play()
 
 func UpdatePlayerPos() -> void:
 	UtilsGlobalVariables.playerPosition = global_position
