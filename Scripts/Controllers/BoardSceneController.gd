cee35a61264320c934c8182f338e79e2101fd45d
Jelmerrr <Jelljel@hotmail.com>
2026-03-25 10:40:46 +0100

    Added failsafe in case the camera bug happens, always forcing the correct camera
    Hopefully this fixes it

--- Scripts/Controllers/BoardSceneController.gd
+++ Scripts/Controllers/BoardSceneController.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+@onready var camera_2d: Camera2D = $"Control/SubViewportContainer/SubViewport/Player, Enemies & Attacks/Camera2D"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -7,6 +8,7 @@ func _ready() -> void:
 	SignalBus.Start_Planning_Phase.emit()
 	AudioControllerScene.currentState = UtilsGlobalEnums.musicPlayerState.Stage1
 	AudioControllerScene.playMusic()
+	camera_2d.make_current()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
