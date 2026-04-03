6ef6a89c67610dd2ebb7b0247208d7cbd119d006
Jelmerrr <Jelljel@hotmail.com>
2026-04-03 10:41:16 +0200

    Small tweaks to page activation tween

--- Scripts/Controllers/PagesContainerUIController.gd
+++ Scripts/Controllers/PagesContainerUIController.gd
@@ -86,7 +86,8 @@ func _input(event):
 func Highlight() -> void:
 	var tween = get_tree().create_tween()
 	tween.set_parallel(true)
-	tween.set_ease(Tween.EASE_OUT)
+	tween.set_ease(Tween.EASE_IN)
+	tween.set_trans(Tween.TRANS_BACK)
 	tween.tween_method(Set_Shader_ColorParam, Color("bac7c7"), Color("150e10"), 0.5)
 	tween.tween_method(Set_shader_WidthParam, 5.0, 0.0, 0.5)
 
