5815c7e24b0bccbfdde192ff0e193c0f1e846ad5
Jelmerrr <Jelljel@hotmail.com>
2026-05-15 11:19:04 +0200

    Initial creation of the resource

---
+++ Resources/Modifiers/res_Modifiers.gd
@@ -0,0 +1,30 @@
+extends Resource
+
+class_name ModifiersResource
+
+var damageModifiersDict = { #In percentile
+	"increasedDamage": {"Base": 100.0, "Current": 100.0, "Tag": "Global"},
+	"increasedSpellDamage": {"Base": 100.0, "Current": 100.0, "Tag": "Spell"},
+	"increasedLightningDamage": {"Base": 100.0, "Current": 100.0, "Tag": "Lightning"},
+	"increasedFireDamage": {"Base": 100.0, "Current": 100.0, "Tag": "Fire"},
+	"increasedColdDamage": {"Base": 100.0, "Current": 100.0, "Tag": "Cold"},
+	"increasedDamageOverTime": {"Base": 100.0, "Current": 100.0, "Tag": "DamageOverTime"},
+	"increasedAreaDamage": {"Base": 100.0, "Current": 100.0, "Tag": "AreaOfEffect"},
+}
+
+var ailmentModifiersDict = {
+	"igniteChance": {"Base": 5.0, "Current": 5.0, "Tag": "igniteChance"},
+	"igniteEffect": {"Base": 100.0, "Current": 100.0, "Tag": "igniteEffect"},
+	"igniteBaseDuration": {"Base": 4.0, "Current": 4.0, "Tag": "igniteDuration"},
+	"igniteDurationIncrease": {"Base": 100.0, "Current": 100.0, "Tag": "igniteDurationIncrease"},
+	"ignitePercentageOfHitDamage": {"Base": 10.0, "Current": 10.0, "Tag": "igniteHitDamage"},
+	"shockChance": {"Base": 5.0, "Current": 5.0, "Tag": "shockChance"},
+	"shockEffect": {"Base": 100.0, "Current": 100.0, "Tag": "shockEffect"},
+	"shockBaseDamageIncrease": {"Base": 110.0, "Current": 110.0, "Tag": "shockDamage"},
+	"shockTriggerAmount": {"Base": 5.0, "Current": 5.0, "Tag": "shockAmount"},
+	"chillChance": {"Base": 10.0, "Current": 10.0, "Tag": "chillChance"},
+	"chillEffect": {"Base": 100.0, "Current": 100.0, "Tag": "chillEffect"},
+	"chillBaseDuration": {"Base": 5.0, "Current": 5.0, "Tag": "chillDuration"},
+	"chillDurationIncrease": {"Base": 100.0, "Current": 100.0, "Tag": "chillDuration"},
+	"chillBaseSlowdown": {"Base": 10.0, "Current": 10.0, "Tag": "chillBaseSlowdown"},
+}
