ff8b8afd35d579b7daaa205b5229d1083541de6c
Jelmerrr <Jelljel@hotmail.com>
2026-06-02 13:35:56 +0200

    Expanding initial population algorithm to create a map node instance for each possible location

--- Scripts/Controllers/MapGenerationController.gd
+++ Scripts/Controllers/MapGenerationController.gd
@@ -2,16 +2,31 @@ extends Node2D
 
 var currentStageMap: MapInstanceResource = MapInstanceResource.new()
 
+func _ready() -> void:
+	InitializeMapGenForStage()
+
 func InitializeMapGenForStage() -> void:
 	ClearCurrentStageMap()
+	PopulateMapData()
 
 func ClearCurrentStageMap() -> void:
 	currentStageMap.maxRandomizedSize = Vector2(8,5) #This is base size, allowing for 8 encounters + 1 boss with a heigth of 5
 	currentStageMap.mapData.clear()
 
 func PopulateMapData() -> void:
-	var currentNodeAttempt: Vector2
-	currentStageMap.mapData[currentNodeAttempt] = SetNodeData(currentNodeAttempt)
+	var currentNodeAttempt: Vector2 = Vector2(1,1)
+	var populating: bool = true
+	while populating:
+		while currentNodeAttempt.y <= currentStageMap.maxRandomizedSize.y:
+			currentStageMap.mapData[currentNodeAttempt] = SetNodeData(currentNodeAttempt)
+			currentNodeAttempt.y += 1
+		if currentNodeAttempt.x < currentStageMap.maxRandomizedSize.x:
+			currentNodeAttempt.x += 1
+			currentNodeAttempt.y = 1
+			continue
+		populating = false
+		break
+	print(currentStageMap.mapData)
 
 func SetNodeData(nodeLocation: Vector2) -> MapNodeResource:
 	var result: MapNodeResource = MapNodeResource.new()
