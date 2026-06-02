d364a1a419d33184e1f01bed7b4a8dffd43ed437
Jelmerrr <Jelljel@hotmail.com>
2026-06-02 13:00:36 +0200

    Initial node population algorithm

--- Scripts/Controllers/MapGenerationController.gd
+++ Scripts/Controllers/MapGenerationController.gd
@@ -1,10 +1,19 @@
 extends Node2D
 
-var currentStageMap: MapInstanceResource
+var currentStageMap: MapInstanceResource = MapInstanceResource.new()
 
 func InitializeMapGenForStage() -> void:
 	ClearCurrentStageMap()
 
 func ClearCurrentStageMap() -> void:
-	currentStageMap.maxRandomizedSize = Vector2(8,5) #This is base size, allowing for 8 encounters + 1 boss
+	currentStageMap.maxRandomizedSize = Vector2(8,5) #This is base size, allowing for 8 encounters + 1 boss with a heigth of 5
 	currentStageMap.mapData.clear()
+
+func PopulateMapData() -> void:
+	var currentNodeAttempt: Vector2
+	currentStageMap.mapData[currentNodeAttempt] = SetNodeData(currentNodeAttempt)
+
+func SetNodeData(nodeLocation: Vector2) -> MapNodeResource:
+	var result: MapNodeResource = MapNodeResource.new()
+	result.mapPosition = nodeLocation
+	return result
