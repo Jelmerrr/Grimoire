8e6029626b2b8405405b7345b9f93890dbd77bed
Jelmerrr <Jelljel@hotmail.com>
2026-06-03 10:39:57 +0200

    Improvements to node population to prevent steep connections

--- Scripts/Controllers/MapGenerationController.gd
+++ Scripts/Controllers/MapGenerationController.gd
@@ -16,9 +16,11 @@ func ClearCurrentStageMap() -> void:
 func PopulateMapData() -> void:
 	var currentNodeAttempt: Vector2 = Vector2(1,1)
 	var populating: bool = true
+	var fullMapIsNodeEmptyArray: Array[Array]
 	while populating:
-		var emptyNodesInRow: int = UtilsRngHandler.rng.randi_range(0, currentStageMap.maxRandomizedSize.y - 2)
+		var emptyNodesInRow: int = UtilsRngHandler.rng.randi_range(0, int(currentStageMap.maxRandomizedSize.y))
 		var isNodeEmptyForStage: Array[bool] = PopulateEmptyNodeData(emptyNodesInRow)
+		fullMapIsNodeEmptyArray.append(isNodeEmptyForStage)
 		while currentNodeAttempt.y <= currentStageMap.maxRandomizedSize.y:
 			var preliminaryNodeType: UtilsGlobalEnums.nodeTypes
 			if isNodeEmptyForStage[currentNodeAttempt.y - 1]:
@@ -33,6 +35,7 @@ func PopulateMapData() -> void:
 			continue
 		populating = false
 		break
+	print(fullMapIsNodeEmptyArray)
 	for item in currentStageMap.mapData:
 		print(currentStageMap.mapData[item].mapPosition)
 		print(UtilsGlobalEnums.nodeTypes.keys()[currentStageMap.mapData[item].nodeType])
@@ -52,4 +55,20 @@ func PopulateEmptyNodeData(emptyNodesInRow: int) -> Array[bool]:
 	while result.size() < currentStageMap.maxRandomizedSize.y:
 		result.append(false)
 	UtilsRngHandler.shuffleArray(result)
+	result = PreventTwoStepJump(result)
 	return result
+
+func PreventTwoStepJump(array: Array[bool]) -> Array[bool]:
+	#Prevent edges having 2 empty nodes in sequence
+	if array[0] && array[1]:
+		var rngresult = UtilsRngHandler.rng.randi_range(0, 1)
+		array[rngresult] = !array[rngresult]
+	if array[array.size() - 1] && array[array.size() - 2]:
+		var rngresult = UtilsRngHandler.rng.randi_range(array.size() - 2, array.size() - 1)
+		array[rngresult] = !array[rngresult]
+	#Prevent 3 empty nodes from generating in sequence
+	for i in array.size() - 2:
+		if array[i] && array[i+1] && array[i+2]:
+			var rngresult = UtilsRngHandler.rng.randi_range(i, i+2)
+			array[rngresult] = !array[rngresult]
+	return array
